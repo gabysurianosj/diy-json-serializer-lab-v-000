47139c301d60b1d6924f485f88d4fde0188d1ae9
Gabriela Suriano <surianogabriela@gmail.com>
2018-11-05 13:36:00 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,6 +17,10 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def create
     Product.create(product_params)
     redirect_to products_path
